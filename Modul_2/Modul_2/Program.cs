fbb4ca581245471825f9430e32f3f1ba1bd5432f
Ninelninel87 <135541573+Ninelninel87@users.noreply.github.com>
2023-06-05 21:44:18 +0300

    Create Program.cs

---
+++ Modul_2/Modul_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Modul_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string fullName = "Иванов Иван";
+            byte age = 20;
+            string email = "ivanovivan@gmail.com";
+            double scoreProgramming = 4.4;
+            double scoreMath = 3.2;
+            double scorePhysics = 3.7;
+            double scoreSum = scoreProgramming + scoreMath + scorePhysics;
+            double scoreGpa = scoreSum / 3;
+            
+            Console.WriteLine($"{"ФИО:",20} {fullName,20}");
+            Console.WriteLine($"{"Возраст:",20} {age,20}");
+            Console.WriteLine($"{"E-mail:",20} {email,20}");
+            Console.WriteLine($"{"Программирование:",20} {scoreProgramming,20}");
+            Console.WriteLine($"{"Математика:",20} {scoreMath,20}");
+            Console.WriteLine($"{"Физика:",20} {scorePhysics,20}");
+            Console.WriteLine($"{"Сумма балов:",20} {scoreSum,20}");
+            Console.WriteLine($"{"Средний балл:",20} {scoreGpa.ToString("#.##"),20}");
+
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
